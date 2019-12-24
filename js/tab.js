79c0b359057a2162bba3fd5b07abaf0da82f9d14
Željko <ozeljko@hotmail.com>
2019-12-24 15:30:29 +0100

    update

--- js/tab.js
+++ js/tab.js
@@ -1,14 +1,16 @@
+
 var btns = document.getElementsByClassName('btn_cv');
 var panels = document.getElementsByClassName('panel');
 var index = document.getElementById('indicator_index');
 
 function tab(btn_cv, panel){
-  for (var i = 0;i<btns.lenght;i++){
+  for (var i = 0;i<btns.length;i++){
     if(i==btn_cv){
       panels[panel].style.display='block';
     }else{
         panels[i].style.display='none';
       }
   }
-  index.style.left = String(btn_cv*33) + "%";
+  index.style.left=String(btn_cv*33) + "%";
+
 }
